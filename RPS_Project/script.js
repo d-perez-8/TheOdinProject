61801a5f5b27038dbf872d6aaf7c1d47bb922fb5
Danny Perez <xdanny28x@gmail.com>
2022-08-27 08:43:21 -0700

    clean up extra spaces and delete unused code

--- RPS_Project/script.js
+++ RPS_Project/script.js
@@ -14,11 +14,9 @@ function computerChoice() {
     return OUTCOMES[Math.floor(Math.random() * OUTCOMES.length)];
 }
 
-// plays one round
 function playRound(playerSelection, computerSelection = computerChoice()) {
     const results = document.querySelector('#results')
 
-    // logic for game
     if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || 
         (playerSelection == 'Paper' && computerSelection == 'Rock') || 
         (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
@@ -31,11 +29,8 @@ function playRound(playerSelection, computerSelection = computerChoice()) {
             }
 
     } else if (playerSelection === computerSelection) {
-
         results.textContent = `You tie! ${playerSelection} draws ${computerSelection}. Player: ${playerScore} Computer: ${computerScore}`;
-
     } else {
-
         computerScore++;
         results.textContent = `You lose! ${playerSelection} loses to ${computerSelection}. Player: ${playerScore} Computer: ${computerScore}`;
 
@@ -44,23 +39,3 @@ function playRound(playerSelection, computerSelection = computerChoice()) {
             }
     }
 }
-/*
-// plays five games and keeps score
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log("Player: " + playerScore);
-        console.log("Computer: " + computerScore);
-    } if (playerScore > computerScore) {
-        return "Congrats! You win!";
-    } else if (playerScore < computerScore){
-        "Unlucky, you lose."
-    } else {
-        return "It's a tie!";
-    }
-}
-*/
-// console.log(game());
