e79e7620c6204dc7fb4b992909fb6bccf3699e1b
Danny Perez <xdanny28x@gmail.com>
2022-08-27 08:05:35 -0700

    display results on page instead of console

--- RPS_Project/script.js
+++ RPS_Project/script.js
@@ -1,42 +1,42 @@
 const btns = document.querySelectorAll('button');
 
+// gets the value from the button
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        playRound(btn.value);
+    })
+});
+
 function computerChoice() {
     const OUTCOMES = ['Rock', 'Paper', 'Scissors'];
     return OUTCOMES[Math.floor(Math.random() * OUTCOMES.length)];
 }
 
-// gets the value from the button
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        const btnValue = btn.value;
-        playRound(btnValue);
-    })
-});
-
 // plays one round
 function playRound(playerSelection, computerSelection = computerChoice()) {
+    const results = document.querySelector('#results')
 
     // logic for game
     if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || 
         (playerSelection == 'Paper' && computerSelection == 'Rock') || 
         (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
 
-        console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
 
     } else if (playerSelection === computerSelection) {
-        console.log(`You tie! ${playerSelection} draws ${computerSelection}`);
+        results.textContent = `You tie! ${playerSelection} draws ${computerSelection}.`;
     } else {
-        console.log(`You lose! ${playerSelection} loses to ${computerSelection}`);
+        results.textContent = `You lose! ${playerSelection} loses to ${computerSelection}.`;
     }
 }
-/*
+
 // plays five games and keeps score
 function game() {
     playerScore = 0;
     computerScore = 0;
 
     for (let i = 0; i < 5; i++) {
-        playRound();
+        playRound(btn.value);
         console.log("Player: " + playerScore);
         console.log("Computer: " + computerScore);
     } if (playerScore > computerScore) {
@@ -49,4 +49,3 @@ function game() {
 }
 
 // console.log(game());
-*/
