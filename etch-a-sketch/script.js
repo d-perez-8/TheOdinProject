4ce68de018285aac44b741aff37913ea81f0e796
Danny Perez <xdanny28x@gmail.com>
2022-08-30 11:28:28 -0700

    add mouseover effect to change grid to black

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -2,18 +2,24 @@ const btns = document.querySelectorAll('button');
 const sketchpad = document.querySelector('.sketchpad');
 let grid = 16;
 
-// create sketchpad
-// function makeSketchpad (grid) {
-    for (let i = 0; i < grid * grid; i++) {
-        const newGrid = document.createElement('div');
-        newGrid.classList.add('cell')
-        sketchpad.appendChild(newGrid);
-    }
-//}
+// deafult sketch pad 
+for (let i = 0; i < grid * grid; i++) {
+    const newGrid = document.createElement('div');
+    newGrid.classList.add('cell');
+    sketchpad.appendChild(newGrid);
+}
+
+// create mouseover effect
+const gridCell = document.querySelectorAll('.cell');
+gridCell.forEach(square => {
+    square.addEventListener('mouseover', e => {
+        square.style.backgroundColor = 'black';
+    })
+});
 
 // make sure buttons work
 btns.forEach(btn => {
     btn.addEventListener('click', e => {
         console.log("Clicked!");
     })
-})
+});
