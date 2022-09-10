ef94a882ec7a0f1fa6194d21fb3f52f20b8e468e
Danny Perez <xdanny28x@gmail.com>
2022-09-10 09:20:56 -0700

    add switch case and make operators easier to read

--- calculator-project/script.js
+++ calculator-project/script.js
@@ -1,21 +1,67 @@
-myArray = [20, 2, 2]
+let currentOperator = null;
+let firstOperand = '';
+let secondOperand = '';
 
-const addition = function(array) {
-    return array.reduce((total, current) => total + current);
+const btns = document.querySelectorAll('button');
+const displayValue = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear');
+const addBtn = document.querySelector('.addition');
+const subtractBtn = document.querySelector('.subtraction');
+const multiplyBtn = document.querySelector('.multiplication');
+const divideBtn = document.querySelector('.division');
+const equalBtn = document.querySelector('.equal');
+const operatorBtn = document.querySelectorAll('.operator');
+
+clearBtn.addEventListener('click', clearDisplay);
+// equalBtn.addEventListener('click', operate);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        firstOperand += btn.value;
+        displayValue.innerHTML = firstOperand;
+        if (btn == operatorBtn) {
+            secondOperand += btn.value;
+            displayValue.innerHTML = secondOperand
+        } else if (btn == equalBtn) {
+            operate;
+        }
+    })
+});
+
+function clearDisplay() {
+    firstOperand = '';
+    secondOperand = '';
+    currentOperator = null;
+    displayValue.innerHTML = firstOperand;
+}
+
+// OPERATEORS //
+const addition = function(a, b) {
+    a + b;
 };
-
-const subtraction = function(array) {
-    return array.reduce((total, current) => total - current);
+const subtraction = function(a, b) {
+    a - b;
 };
-
-const multiplication = function(array) {
-    return array.reduce((total, current) => total * current, 1);
+const multiplication = function(a, b) {
+    a * b;
 };
-
-const division = function(array) {
-    return array.reduce((total, current) => total / current);
+const division = function(a, b) {
+    a / b;
 };
 
 const operate = function(operator, a, b) {
-    return operator([a, b]);
-}
+    switch (operator) {
+        case addBtn:
+            return addition(a, b);
+        case subtractBtn:
+            return subtraction(a, b);
+        case multiplyBtn:
+            return subtraction(a, b);
+        case divideBtn:
+            if (b === 0) {
+                return null
+            } else {
+                division([a, b])
+            }
+    }
+};
