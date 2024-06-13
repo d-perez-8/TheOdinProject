c5c340aaf3fc63303f31c30d66162b89f4793a85
Daniel Perez <dandevelopsweb@gmail.com>
2024-06-13 14:57:19 -0700

    add shuffling logic

--- react/memory-card-app/src/App.jsx
+++ react/memory-card-app/src/App.jsx
@@ -1,4 +1,3 @@
-// TODO: Make each card a button and resets if you click a duplicate
 // TODO: Make cards shuffle after each click
 // TODO: Add card flip animation when a card is clicked
 
@@ -13,12 +12,24 @@ function App() {
     const [score, setScore] = useState(0);
     const [personal, setPersonal] = useState(Number(localStorage.getItem("personal")));
     const [clickedPlayers, setClickedPlayers] = useState([]);
+    const [shuffledPlayers, setShuffledPlayers] = useState([...players]);
 
     // updates local storage when personal best changes
     useEffect(() => {
       localStorage.setItem('personal', personal)
-  }, [personal])
+    }, [personal])
 
+    // handles shuffle array logic
+    function shuffler(arr) {
+      for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+      }
+      return arr
+    }
+
+
+    // handles game logic
     function handleCardClick(playerName) {
       if (clickedPlayers.includes(playerName)) {
         setScore(0);
@@ -30,9 +41,10 @@ function App() {
           setPersonal(s => s + 1)
         }
       }
+      setShuffledPlayers(shuffler([...shuffledPlayers]))
     }
 
-    const list = players.map(player => (
+    const list = shuffledPlayers.map(player => (
       <li key= { player.id }>
         <button onClick={() => handleCardClick(player.name)}>
           <Card name={player.name} src={player.img} />
